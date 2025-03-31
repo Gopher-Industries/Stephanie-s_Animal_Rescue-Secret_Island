2abff11907f888f9c1b7517223e8fdb0c1844d3d
JackBoakes <jboakes@deakin.edu.au>
2025-03-31 21:11:13 +1100

    Dynamic Voiceline Functionality
    Integrated the voice line functionality for movement in the.
    
    Changed the charactermover isMoving variable to be able to privately set it within the class and read only externally.
    
    Can easily add voice line assets into the list via the voice line manager object.
    
    Needs further work to implement into all 3D movable scenes.

---
+++ Assets/Code/VoiceLineManager.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+
+public class VoicelineManager : MonoBehaviour
+{
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip playVoiceline;
+    [SerializeField] private AudioClip[] movementVoicelines;
+    [SerializeField] private CharacterMover characterMoverScript;
+
+    private const float VOICELINE_COOLDOWN = 10f;
+    private float lastPlayedTime = 0f;
+
+
+    void Start()
+    {
+        lastPlayedTime = Time.time - VOICELINE_COOLDOWN;
+
+        audioSource = GetComponent<AudioSource>();
+
+        if (characterMoverScript == null)
+        {
+            Debug.LogError("ClickToMove script not assigned!");
+        }
+
+        PlayInitialVoiceline();
+    }
+
+    // Check if the voiceline is on cooldown
+    public bool CanPlayVoiceline()
+    {
+        return (Time.time - lastPlayedTime) >= VOICELINE_COOLDOWN;
+    }
+
+    // Play the initial voiceline when the player presses play
+    public void PlayInitialVoiceline()
+    {
+        Debug.Log("PlayInitialVoiceline method called.");
+        if (CanPlayVoiceline())
+        {
+            Debug.Log("Playing initial voiceline.");
+            audioSource.PlayOneShot(playVoiceline);
+            lastPlayedTime = Time.time;
+        }
+        else
+        {
+            Debug.Log("Initial voiceline failed to play");
+        }
+    }
+
+    // Plays a random voiceline if cooldown has passed and the player is moving
+    public void PlayMovementVoiceline()
+    {
+        if (movementVoicelines == null || movementVoicelines.Length == 0) { return; }
+
+        if (characterMoverScript != null && characterMoverScript.isMoving && CanPlayVoiceline())
+        {
+            int randomIndex = Random.Range(0, movementVoicelines.Length);
+            audioSource.PlayOneShot(movementVoicelines[randomIndex]);
+
+            lastPlayedTime = Time.time;
+        }
+    }
+
+
+    void Update()
+    {
+        PlayMovementVoiceline();
+    }
+}
